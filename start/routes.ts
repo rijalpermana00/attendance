e3c9eec6393e58c205bedaccb9fc731d988f480f
rijal <rijal@neuronworks.co.id>
2022-11-27 21:20:06 +0700

    update route

--- start/routes.ts
+++ start/routes.ts
@@ -20,10 +20,12 @@
 
 import Route from '@ioc:Adonis/Core/Route'
 import HealthCheck from '@ioc:Adonis/Core/HealthCheck'
+import { DateTime } from 'luxon'
+import User from 'App/Models/User'
 
-Route.get('/', async ({ view }) => {
-  return view.render('welcome')
-})
+// Route.get('/', async ({ view }) => {
+//   return view.render('welcome')
+// })
 
 Route.get('health', async ({ response }) => {
   const report = await HealthCheck.getReport()
@@ -32,3 +34,90 @@ return report.healthy
 ? response.ok(report)
 : response.badRequest(report)
 })
+
+Route.get('/', async () => {
+	return { 
+	   hello: `let's get started`
+   }
+})
+
+Route.post('register', 'UsersController.register')
+Route.post('login', 'UsersController.login')
+Route.get('logout', 'UsersController.logout').middleware("auth:api")
+
+Route.group(() => {
+	Route.get('/users', 'UsersController.index')
+	Route.get('/posts', 'PostsController.index')
+	Route.group(() => {
+		Route.post('create', 'User/HouseHoldsController.store').middleware("auth:api")
+	}).prefix('/household')
+	
+	Route.group(() => {
+		Route.post('create', 'Transaction/CategoriesController.store').middleware("auth:api")
+	}).prefix('/category')
+	
+	Route.group(() => {
+		Route.post('create', 'Transaction/SubsController.store').middleware("auth:api")
+	}).prefix('/subcategory')
+	
+	Route.group(() => {
+		Route.post('create', 'TransactionsController.store').middleware("auth:api")
+		Route.get('/', 'TransactionsController.index').middleware("auth:api")
+		Route.post('update', 'TransactionsController.update').middleware("auth:api")
+		Route.get('list', 'TransactionsController.list').middleware("auth:api")
+	}).prefix('/transaction')
+	
+	
+}).prefix('/api')
+
+Route.get('/index', 'UsersController.index').middleware("auth:api")
+
+Route.post('send-email', 'UsersController.sendEmail')
+Route.post('forgot-password', 'UsersController.forgotPassword')
+
+Route.get('/verify/:email', async ({ request }) => {
+
+	if (request.hasValidSignature()) {
+		
+		const query = request.url()
+		const url = query.split("/");
+
+		const user = await User.findByOrFail('email', url[2])
+		user.updatedAt = DateTime.local() // Luxon dateTime is used
+		user.auth_dtm = DateTime.local() // Luxon dateTime is used
+		user.status_id = 1
+
+		await user.save();
+
+		return {
+			code:'0',
+			info:'Email Verified'
+		}
+		
+	}
+
+  	return {code:'1',info:'Url is not valid'}
+	  
+}).as('verifyEmail')
+
+Route.get('/locked/:email', async ({ request }) => {
+
+	if (request.hasValidSignature()) {
+		
+		const query = request.url()
+		const url = query.split("/");
+
+		const user = await User.findByOrFail('email', url[2])
+		user.updatedAt = DateTime.local() // Luxon dateTime is used
+		user.status_id = 1
+		user.attempts = 0
+
+		await user.save();
+
+		return {code:'0',info:'Account Unlocked'}
+		
+	}
+
+  	return {code:'1',info:'Url is not valid'}
+	  
+}).as('lockedAccount')
