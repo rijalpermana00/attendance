84b959186f828297c22234828d6a1b8ba75aee44
rijal <rijal@neuronworks.co.id>
2022-11-29 23:45:02 +0700

    update

--- app/Controllers/Http/ShiftsController.ts
+++ app/Controllers/Http/ShiftsController.ts
@@ -2,10 +2,12 @@ import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 
 export default class ShiftsController {
     
-    public async index ({ request, auth }: HttpContextContract) {
+    public async index ({ request }: HttpContextContract) {
      
         let req = request.only(['code','info','data']);
         console.log(req);
+        
+        
         
     }
 }
