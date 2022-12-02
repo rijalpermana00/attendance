856a4b0f7628f64ab57ff82ef9c0fbfd3ba11e57
rijal <rijal@neuronworks.co.id>
2022-12-02 23:50:21 +0700

    update location

--- app/Controllers/Http/UsersController.ts
+++ app/Controllers/Http/UsersController.ts
@@ -1,5 +1,4 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
-import { schema, rules, validator } from '@ioc:Adonis/Core/Validator'
 import User from 'App/Models/User'
 import CreateUser from 'App/Mailers/CreateUser'
 import { Roles } from 'App/Enums/Roles';
